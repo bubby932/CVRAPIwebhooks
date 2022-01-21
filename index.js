1ce921c4f68cb19c13fe95fcedcd579bb511fc04
bubby932 <tvrec10@gmail.com>
2022-01-21 11:47:30 -0500

    yeah

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { exec } = require("child_process");
+const crypto = require('crypto');
+require('dotenv').config();
 
 const app = express();
 const PORT = 3001;
@@ -13,6 +15,15 @@ app.post("/github", (req, res) => {
 
      if(typeof req.body.ref !== 'string') return res.status(200).send();
      else if (req.body.ref !== "refs/heads/production") return res.status(200).send();
+
+     var hmac = crypto.createHmac('sha256', process.env.GITHUB_SECRET);
+
+     var data = hmac.update(req.body);
+
+     var gen_hmac = data.digest('hex');
+
+     if(gen_hmac !== req.headers["X-Hub-Signature-256"]) return res.status(403).send("Invalid secret key!");
+
      exec("cd ../VigorXRAPI; git pull", (error, stdout, stderr) => {
      if (error) {
           console.log(`error: ${error.message}`);
