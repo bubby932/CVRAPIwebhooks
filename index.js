a9450e583b883692d2f57203a630a641e167a2ba
bubby932 <tvrec10@gmail.com>
2022-01-21 11:33:35 -0500

    Still more testing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { exec } = require("child_process");
 
 const app = express();
 const PORT = 3001;
@@ -9,6 +10,20 @@ app.use(bodyParser.json());
 app.post("/github", (req, res) => {
      console.log(req.body);
      res.status(200).send();
+
+     if(typeof req.body.ref !== 'string') return res.status(200).send();
+     else if (req.body.ref !== "refs/heads/production") return res.status(200).send();
+     exec("cd ../VigorXRAPI; git pull", (error, stdout, stderr) => {
+     if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+     }
+     if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+     }
+     console.log(`stdout: ${stdout}`);
+     });
 });
 
 app.get("/health", (req, res) => {
