958b7647c0c2427339c30f7fe542eeab843acc38
bubby932 <tvrec10@gmail.com>
2022-01-21 11:51:08 -0500

    yse

--- index.js
+++ index.js
@@ -11,30 +11,30 @@ app.use(bodyParser.json());
 
 app.post("/github", (req, res) => {
      console.log(req.headers);
-     res.status(200).send();
-
+     
      if(typeof req.body.ref !== 'string') return res.status(200).send();
      else if (req.body.ref !== "refs/heads/production") return res.status(200).send();
-
+     
      var hmac = crypto.createHmac('sha256', process.env.GITHUB_SECRET);
-
+     
      var data = hmac.update(req.body);
-
+     
      var gen_hmac = data.digest('hex');
-
+     
      if(gen_hmac !== req.headers["x-hub-signature-256"]) return res.status(403).send("Invalid secret key!");
-
+     
      exec("cd ../VigorXRAPI; git pull", (error, stdout, stderr) => {
-     if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-     }
-     if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return;
-     }
-     console.log(`stdout: ${stdout}`);
+          if (error) {
+               console.log(`error: ${error.message}`);
+               return;
+          }
+          if (stderr) {
+               console.log(`stderr: ${stderr}`);
+               return;
+          }
+          console.log(`stdout: ${stdout}`);
      });
+     res.status(200).send();
 });
 
 app.get("/health", (req, res) => {
