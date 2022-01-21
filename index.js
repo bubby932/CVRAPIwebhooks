ff7410f41d00a7631cd727e87b5fd637f6b32a25
bubby932 <tvrec10@gmail.com>
2022-01-21 11:55:30 -0500

    idk

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post("/github", (req, res) => {
      
      var hmac = crypto.createHmac('sha256', process.env.GITHUB_SECRET);
      
-     var data = hmac.update(req.body);
+     var data = hmac.update(req.body.toString());
      
      var gen_hmac = data.digest('hex');
      
